1414a3f0028f992a6589f4353182fcc8e846e29e
Nolan <60010605+nolngo@users.noreply.github.com>
2020-10-06 03:06:59 -0700

    revised js file

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 let button = document.getElementById("button");
 button.addEventListener("click", function(){
-    let total = document.getElementById("total").value;
-    let percent = document.getElementById("gratuity_percentage").value;
+    let orderTotal = document.getElementById("total").value;
+    let percentTip = document.getElementById("gratuity_percentage").value;
 
-    let gratuity = document.getElementById("gratuity_amount").value = total * (percent * 0.01);
-    
-    document.getElementById("grand_total").value = parseFloat(total) + parseFloat(gratuity); 
+    let amountTip = document.getElementById("gratuity_amount").value = 0.01 * percentTip * orderTotal;
+    document.getElementById("grand_total").value = parseFloat(orderTotal) + parseFloat(amountTip);
 })
